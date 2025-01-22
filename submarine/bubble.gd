5114736f6f6bcd0e7e5b05fcc8ba634b5ca5af3b
Efraim Marcatto <efraimmarcatto@gmail.com>
2025-01-22 15:29:44 -0300

    feat: Bubble adjustments
    Fix bubble relative speed
    Adds the conditional collisions
    Fix player movements

--- submarine/bubble.gd
+++ submarine/bubble.gd
@@ -1,33 +1,33 @@
 extends Area2D
 
-@export var speed: float = 100.0
+const MIN_SCALE = 0.25
+const MAX_SCALE = 1.5
+const EXPLODE_SCALE = 2.5
+const MIN_SPEED = 30
+const MAX_SPEED = 200
 
+var speed: float = 100.0
 var is_able_to_move = false
+var is_holding_something = false
 var direction: Vector2 = Vector2.RIGHT
 
-var initial_scale: Vector2 = Vector2(0.25,0.25)
-
-const MAX_SCALE = 1.5
-
 func _ready() -> void:
-	scale = initial_scale
+	scale = Vector2(MIN_SCALE, MIN_SCALE)
+
 
 func _process(delta) -> void:
 	if (is_able_to_move):
 		translate(direction * speed * delta)
+
 
 func increase_scale(mor_scale:float) -> void:
 	if scale.x >= MAX_SCALE:
 		scale = Vector2(MAX_SCALE,MAX_SCALE)
 	else:
 		scale += Vector2(mor_scale,mor_scale)
-		
-	if scale.x >= MAX_SCALE:
-		speed = 100
-	elif scale.x >= 1:
-		speed = 150
-	else: 
-		speed = 250
+	if !is_holding_something:
+		speed = clamp((MAX_SCALE - scale.x) * (MAX_SPEED / MAX_SCALE), MIN_SPEED, MAX_SPEED)
+
 
 func start_move() -> void:
 	if $AnimationPlayer.is_playing() :
@@ -40,11 +40,33 @@ func explode() -> void:
 	is_able_to_move = false
 	$AnimationPlayer.play("explode")
 	await $AnimationPlayer.animation_finished
-	queue_free()
+	call_deferred("queue_free")
+
 
 func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
 	queue_free()
 
+
 func _on_area_entered(area: Area2D) -> void:
 	if area.is_in_group("Bubbles"):
+			if scale.x > area.scale.x and scale.x < EXPLODE_SCALE:
+				print(area.scale / MAX_SCALE)
+				scale += area.scale / MAX_SCALE
+			else:
+				explode()
+
+	elif area.is_in_group("Floatable"):
+		is_holding_something = true
+		is_able_to_move = false
+		speed = 100
+		direction = Vector2.UP
+		area.global_position = $CenterMarker.global_position
+		area.call_deferred("reparent", self)
+		area.set_deferred("monitorable", false)
+		area.scale = Vector2(.8,.8)
+		area.z_index = -1
+		await get_tree().create_timer(.2).timeout
+		is_able_to_move = true
+		
+	else:
 		explode()
--- submarine/submarine.gd
+++ submarine/submarine.gd
@@ -10,6 +10,7 @@ var can_shoot = true
 var delay_shoot = 0.3
 var delay_shoot_buffer = 0
 
+
 func _physics_process(delta: float) -> void:
 	walk()
 	
@@ -17,9 +18,10 @@ func _physics_process(delta: float) -> void:
 	
 	move_and_slide()
 
+
 func walk() -> void:
-	var direction_x = Input.get_axis("ui_left", "ui_right")
-	var direction_y = Input.get_axis("ui_up", "ui_down")
+	var direction_x = Input.get_axis("move_left", "move_right")
+	var direction_y = Input.get_axis("move_up", "move_down")
 	
 	if direction_x:
 		velocity.x = lerp(velocity.x, direction_x * speed, WATER_FRICTION)
@@ -30,7 +32,8 @@ func walk() -> void:
 		velocity.y = lerp(velocity.y, direction_y * speed, WATER_FRICTION)
 	else:
 		velocity.y = move_toward(velocity.y, 0, speed)
-	
+
+
 func shoot(delta) -> void:
 	
 	if can_shoot:
@@ -39,12 +42,12 @@ func shoot(delta) -> void:
 			instance_bullet.global_position = $BulletSpawnerMarker.global_position
 			get_tree().root.add_child(instance_bullet)
 		
-		if Input.is_action_pressed("ui_shoot") && instance_bullet:
+		if Input.is_action_pressed("ui_shoot") && is_instance_valid(instance_bullet):
 			instance_bullet.global_position = $BulletSpawnerMarker.global_position
 			if instance_bullet && instance_bullet.has_method("increase_scale"):
 				instance_bullet.increase_scale(delta/2)
 		
-		if Input.is_action_just_released("ui_shoot") && instance_bullet && instance_bullet.has_method("start_move"):
+		if Input.is_action_just_released("ui_shoot") && is_instance_valid(instance_bullet) && instance_bullet.has_method("start_move"):
 			instance_bullet.start_move()
 			instance_bullet = null
 			can_shoot = false
@@ -53,4 +56,3 @@ func shoot(delta) -> void:
 		if delay_shoot_buffer >= delay_shoot:
 			can_shoot = true
 			delay_shoot_buffer = 0
-		
