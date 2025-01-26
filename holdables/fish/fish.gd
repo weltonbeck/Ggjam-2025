3507da31581be705445f02cc6a2a1c7dc3e46111
welton <tomwb1@gmail.com>
2025-01-26 14:15:51 -0300

    feat: hud in levels

--- holdables/fish/fish.gd
+++ holdables/fish/fish.gd
@@ -1,22 +1,26 @@
 extends Holdable
 
 @export var move = true
+@export var move_verticaly = false
 @export var speed = 150
 @export var face_to_right = true
 
 var direction:Vector2 = Vector2.LEFT
 
 func _ready() -> void:
+	if move_verticaly:
+		direction = Vector2.UP
 	if face_to_right:
-		flip()
+		flip(true)
 
 func _process(delta: float) -> void:
 	if move:
 		translate(direction * delta * speed)
 
-func flip() -> void:
-	$AnimatedSprite2D.flip_h = !$AnimatedSprite2D.flip_h
+func flip(force=false) -> void:
 	direction *= -1
+	if !move_verticaly || force:
+		$AnimatedSprite2D.flip_h = !$AnimatedSprite2D.flip_h
 
 
 func hold(new_parent:Node2D) -> void:
