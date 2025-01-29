cdfb5db8d64d7b7d87c3e4de2ef2f0b30093f843
Efraim Marcatto <efraimmarcatto@gmail.com>
2025-01-29 03:45:38 -0300

    fix: Fixes typo in treasures label

--- levels/level_manager.gd
+++ levels/level_manager.gd
@@ -3,8 +3,9 @@ extends Node2D
 @export var next_level:PackedScene
 @export var treasures_in_level = 1
 signal pause(paused: bool)
-@onready var tresure_count: Label = $LevelMenu/LabelContainer/TresureCount
-@onready var total_tresures: Label = $LevelMenu/LabelContainer/TotalTresures
+@onready var treasure_count: Label = $LevelMenu/LabelContainer/TreasureCount
+@onready var total_treasures: Label = $LevelMenu/LabelContainer/TotalTreasures
+
 
 var treasures_resgated = 0
 
@@ -17,7 +18,7 @@ func _ready() -> void:
 	if get_tree().paused:
 		set_pause()
 
-	total_tresures.text = " / %s" % treasures_in_level
+	total_treasures.text = " / %s" % treasures_in_level
 	GameManager.play_music("level")
 	GameManager.treasure_is_resgated.connect(treasure_is_resgated)
 
@@ -27,7 +28,7 @@ func treasure_is_resgated() -> void:
 	if next_level and treasures_resgated == treasures_in_level :
 		await get_tree().create_timer(.2).timeout
 		GameManager.change_scene(next_level.resource_path)
-	tresure_count.text = str(treasures_resgated)
+	treasure_count.text = str(treasures_resgated)
 
 func set_pause():
 	pause.emit(!get_tree().paused)
